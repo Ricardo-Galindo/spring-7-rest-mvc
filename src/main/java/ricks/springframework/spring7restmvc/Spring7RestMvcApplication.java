eb77c29f6884b0077ce87b38098385669b739474
Ricardo Perez <ricardoperezgalindo@gmail.com>
2026-02-24 14:09:47 -0300

    first commit

---
+++ src/main/java/ricks/springframework/spring7restmvc/Spring7RestMvcApplication.java
@@ -0,0 +1,13 @@
+package ricks.springframework.spring7restmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7RestMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring7RestMvcApplication.class, args);
+    }
+
+}
